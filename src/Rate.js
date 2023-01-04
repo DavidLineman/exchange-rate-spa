b2be70f005344417010df1c8c2ec5c35186fb709
DavidLineman <dline215@gmail.com>
2023-01-03 21:42:44 -0500

    testing state management for multiple form inputs

--- src/Rate.js
+++ src/Rate.js
@@ -1,13 +1,78 @@
 import React from "react";
 import { checkStatus, json } from './utils';
 
+const host = 'api.frankfurter.app';
 
-class Rate extends React.Component {
-  
-  const host = 'api.frankfurter.app';
-  fetch(`https://${host}/latest?amount=10&from=GBP&to=USD`)
-    .then(resp => resp.json())
-    .then((data) => {
-      alert(`10 GBP = ${data.rates.USD} USD`);
+
+class RateFinder extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      amount: '',
+      convertTo: 'USD',
+      convertFrom: 'GBP',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    let { amount, convertTo, convertFrom } = event.target;
+    const value = event.target.value;
+    this.setState({
+      amount: value,
+      convertTo: value,
+      convertFrom: value
     });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let { amount, convertTo, convertFrom } = this.state;
+    alert(this.state.value)
+  }
+   
+    render () {
+      return(
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <div className='container'>
+              <label>Amount</label>
+              <input type="text" name="amount" value={this.state.amount} onChange={this.handleChange}/>
+            </div>
+            <div className='container'>
+              <label>From:</label>
+              <select name="convertFrom" value={this.state.convertFrom} onChange={this.handleChange}>
+                <option value="USD">USD</option>
+                <option value="GBP">GBP</option>
+              </select>
+              <label>To:</label>
+              <select name="convertTo" value={this.state.convertTo} onChange={this.handleChange}>
+                <option value="GBP">GBP</option>
+                <option value="USD">USD</option>
+              </select>
+            </div>
+            <button type='submit' value='submit'>Submit</button>
+          </div>
+          <div className="results"></div>
+    
+        </form>
+      )
+    }
+
+
+
+ 
+
+
+
+
+
+  
+
+
+
 }
+
+export default RateFinder;
